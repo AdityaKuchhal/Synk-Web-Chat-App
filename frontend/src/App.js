dc796fc67993b414184ddea933abe0bc9d332e53
Nirmit <khurananirmit5@gmail.com>
2023-10-01 21:49:05 -0400

    Repo update commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
 import { Route } from "react-router-dom";
-import HomeScreen from "./Screens/HomeScreen";
+// import HomeScreen from "./Screens/HomeScreen";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={HomeScreen} />
-      {/* <Route path="/chats" /> */}
+      <Button colorScheme="blue">Button</Button>
     </div>
   );
 }
