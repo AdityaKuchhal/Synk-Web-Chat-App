0d7cdd618c07ad5156cd66b6e4bdc86364e243a0
Aditya Kuchhal <adikuchhal@gmail.com>
2023-10-02 03:43:59 -0400

    Chat Name render

--- frontend/src/Screens/ChatScreen.js
+++ frontend/src/Screens/ChatScreen.js
@@ -1,17 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const ChatScreen = () => {
+  const [chats, setChats] = useState([]);
+
   const fetchChats = async () => {
     const { data } = await axios.get("/api/chat");
-    console.log(data);
+    setChats(data);
   };
 
   useEffect(() => {
     fetchChats();
   }, []);
 
-  return <div>Chat Page</div>;
+  return (
+    <div>
+      {chats.map((chat) => (
+        <div>{chat.chatName}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ChatScreen;
