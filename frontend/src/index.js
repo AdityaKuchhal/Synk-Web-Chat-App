3d2091557630acb3988aefae3e00f7a6324fa66c
Nirmit <khurananirmit5@gmail.com>
2023-10-03 15:55:46 -0400

    comiting home and chat page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,19 +4,20 @@ import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import ChatProvider from "../../backend/context/chatProvider";
 // import ChatProvider from "../context/chatProvider";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
+  <ChatProvider>
     <BrowserRouter>
       <ChakraProvider>
         <App />
       </ChakraProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </ChatProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
