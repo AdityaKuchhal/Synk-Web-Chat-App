ea3a3438a50c0ea9d1ff990c8a2f63d8dbd37e58
Aditya Kuchhal <adikuchhal@gmail.com>
2023-10-03 16:07:38 -0400

    Frontend Components Implementation

--- frontend/src/Components/scrollableChat.js
+++ frontend/src/Components/scrollableChat.js
@@ -7,7 +7,7 @@ import {
   isSameSenderMargin,
   isSameUser,
 } from "../config/ChatLogics";
-import { ChatState } from "../Context/ChatProvider";
+import { ChatState } from "../Context/chatProvider";
 
 const ScrollableChat = ({ messages }) => {
   const { user } = ChatState();
